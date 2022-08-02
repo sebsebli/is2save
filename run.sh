057334deebcf6d3fcb71d3071d20c5c7ed40a950
Sebastian Lindner <sebastianlindner@sebastians-mbp.speedport.ip>
2022-08-02 09:54:25 +0200

    test

---
+++ run.sh
@@ -0,0 +1,8 @@
+pip install statistics, matplotlib, numpy, tensorflow, random
+cd client
+npm install --legacy-peer-deps
+npm run build
+cd ..
+cd server
+npm install --legacy-peer-deps
+node index.js
